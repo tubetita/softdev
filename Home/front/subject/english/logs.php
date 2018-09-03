abb3f4eaea07dcf9d7ba0984207050d1079721d5
Ryan Garcia <ryngrcia@gmail.com>
2018-09-04 02:47:53 +0800

    Home revised

---
+++ Home/front/subject/english/logs.php
@@ -0,0 +1,16 @@
+<?php
+
+$host='localhost';
+$user='root';
+$pass='';
+$db='tutorfinder';
+
+
+$con = mysqli_connect($host,$user,$pass,$db);
+
+
+$result1 = mysqli_query($con,"SELECT * FROM logs Order by id DESC");
+
+while($extract = mysqli_fetch_array($result1)) {
+	echo "<span>" . $extract['username'] . "</span>: <span>" . $extract['msg'] . "</span><br />";
+}
